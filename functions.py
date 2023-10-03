10a388675cbdf2ae4c8ef7f73b8512f0bc747f92
S G <sgabrielov@gmail.com>
2023-10-02 19:16:52 -0500

    Signed-off-by: S G <sgabrielov@gmail.com>

--- functions.py
+++ functions.py
@@ -1,7 +1,46 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  2 18:52:21 2023
 
-@author: osiri
-"""
+import chess
 
+def convert_fen_to_bitboard(board: chess.Board, cols=None) -> list:
+    
+    
+    """Converts a fen string to a bitboard mapping
+        
+        Parameters
+        ----------
+        fen : str
+            The FEN string
+            
+        Returns
+        -------
+        list
+            A list of bool
+    """
+    
+    # The bitboard mapping is going to use 1 hot encoding - where each bit
+    # corresponds to a specific square, piece, and color
+    
+    outlist = []
+    
+    # encode white pieces
+    # in python-chess chess.WHITE = True and chess.BLACK = False
+    # chess.Pawn = 1, King = 6, etc
+    for i in range(1,7):
+        outlist.extend(board.pieces(i, chess.WHITE).tolist())
+    
+    # encode castling rights for white
+    
+    outlist.append(board.has_castling_rights(chess.WHITE))
+    outlist.append(board.has_queenside_castling_rights(chess.WHITE))
+    
+    # encode black pieces
+    for i in range(1,7):
+        outlist.extend(board.pieces(i, chess.BLACK).tolist())
+    
+    # encode castling rights for black
+    
+    outlist.append(board.has_castling_rights(chess.BLACK))
+    outlist.append(board.has_queenside_castling_rights(chess.BLACK))
+
+    return outlist
